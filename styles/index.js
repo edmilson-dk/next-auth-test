cf7f9b87c7a32002f9beb530a15c14188acb854d
edmilson-dk <droidvidaboa@gmail.com>
2021-04-09 16:26:55 -0300

    feat: created styles

---
+++ styles/index.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-size: 30px;
+  text-align: center;
+  width: 100%;
+  margin-bottom: 30px;
+  color: #333;
+`;
+
+export const SubTitle = styled.h2`
+  font-size: 20px;
+  text-align: center;
+  width: 100%;
+  margin-bottom: 20px;
+  color: #11aa88;
+`;
+
+export const ButtonSignIn = styled.button`
+  border: none;
+  outline: none;
+  width: 180px;
+  border-radius: 5px;
+  margin: 0 auto;
+  display: block;
+  padding: 12px 10px;
+  font-size: 18px;
+  font-weight: 600;
+  color: #Fff;
+  background-color: #4400ff;
+`;
